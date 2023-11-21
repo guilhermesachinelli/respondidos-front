7a2461dbe71e80f660351e68d410804442aa1d7a
Amanda dos Santos Silva <123501998+Amandamoonchild@users.noreply.github.com>
2023-11-21 17:19:45 -0300

    dia1

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
+import styles from "./page.module.css"
+
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.bck}>
+      <div className={styles.container}>
+        <div className={styles.div1}>
+          <h1 className={styles.respondidos}>Respondidos</h1>
+          <h3>Ganhe! Ou seja explodido!</h3>
+          <h2 className={styles.playbutton}>Jogar!</h2>
+        </div>
+        <div className={styles.div2}>
+          <img src="/Logo.png"></img>
+        </div>
+      </div>
+    </div>
   )
 }
