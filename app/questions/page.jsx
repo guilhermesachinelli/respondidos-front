e93157f2948adaa5fba61dd64744246ee9e32c84
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-11-23 14:40:06 -0300

    feat: styled question card

--- app/questions/page.jsx
+++ app/questions/page.jsx
@@ -22,7 +22,7 @@ export default function Page() {
     console.log(questions);
     return (
         <div>
-            <h1>TESTE DOS CRIA</h1>
+
             {
                 dados.length > 0 ? (
                     <div className={styles.question}>
@@ -32,12 +32,11 @@ export default function Page() {
                                         <h1>{questions.question}</h1>
                                         <h2>Catégoria:{questions.category}</h2>
                                         <h3>Nivel de Dificuldade:{questions.difficulty}</h3>
-                                        <h3>{questions.correct}</h3>
-                                        <p>{questions.response1}</p>
-                                        <p>{questions.response2}</p>
-                                        <p>{questions.response3}</p>
-                                        <p>{questions.response4}</p>
-                            
+                                        <p>A - {questions.response1}</p>
+                                        <p>B - {questions.response2}</p>
+                                        <p>C - {questions.response3}</p>
+                                        <p>D - {questions.response4}</p>
+                                        <h3>Resposta Correta é :{questions.correct}</h3>
                                 </div>
                             ))
 
