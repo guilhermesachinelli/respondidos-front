2ad07b5780e2670e3e7319a5dba3e96290b6515b
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-11-23 15:15:40 -0300

    feat: finalized create

--- app/questions/page.jsx
+++ app/questions/page.jsx
@@ -1,12 +1,19 @@
 "use client";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import styles from "./questions.module.css"
-import Link from "next/link";
+import styles from "./questions.module.css";
 import Header from "../components/header/Header";
 export default function Page() {
     const [questions, setQuestions] = useState([]);
     const [dados, setDados] = useState([]);
+    const [question, setQuestion] = useState("");
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [response1, setResponse1] = useState("");
+    const [response2, setResponse2] = useState("");
+    const [response3, setResponse3] = useState("");
+    const [response4, setResponse4] = useState("");
+    const [correct, setCorrect] = useState("");
     useEffect(() => {
         async function fetchQuestions() {
             try {
@@ -19,13 +26,82 @@ export default function Page() {
         }
         fetchQuestions();
     }, [])
-    console.log(questions);
-    console.log(questions);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    try{
+        const response = await axios.post("/api/questions", {question, category, difficulty, response1, response2, response3, response4, correct});
+        setQuestions([...dados, response.data.data]);
+        setQuestion("");
+        setCategory("");
+        setDifficulty("");
+        setResponse1("");
+        setResponse2("");
+        setResponse3("");
+        setResponse4("");
+        setCorrect("");
+    }catch(error){
+        console.error(error);
+    }
+    }
     return (
         <div>
             <Header />
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Questão"
+                        value={question}
+                        onChange={(e) => setQuestion(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Categoria"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Dificuldade"
+                        value={difficulty}
+                        onChange={(e) => setDifficulty(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Resposta 1"
+                        value={response1}
+                        onChange={(e) => setResponse1(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Resposta 2"
+                        value={response2}
+                        onChange={(e) => setResponse2(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Resposta 3"
+                        value={response3}
+                        onChange={(e) => setResponse3(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Resposta 4"
+                        value={response4}
+                        onChange={(e) => setResponse4(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Resposta Correta"
+                        value={correct}
+                        onChange={(e) => setCorrect(e.target.value)}
+                    />
+                    <button type="submit">Enviar</button>
+                </div>
+            </form>
             {
-                dados.length > 0 ? (
+                dados.length  ? (
+                    question ? (
                     <div className={styles.question}>
                         {
                             dados.map((questions) => (
@@ -46,6 +122,11 @@ export default function Page() {
                 ) : (
                     <div>
                         <h1>Carregando...</h1>
+                    </div>
+                )
+                ) : (
+                    <div>
+                        <h1>Não há questões cadastradas</h1>
                     </div>
                 )
             }
