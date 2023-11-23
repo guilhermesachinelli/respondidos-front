59ead14d2e7404ca2def183c3e3768532e69fc23
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-11-23 08:10:10 -0300

    feat: members

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -1,4 +1,6 @@
 import style from './sobrenos.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function SobreNos() {
     return (
@@ -6,7 +8,7 @@ export default function SobreNos() {
             <div className={style.container}>
                 <div className={style.card}>
 <img className={style.card__image} src="https://pbs.twimg.com/profile_images/1366977708115300353/1qo2VGKi_400x400.jpg"/>
-                    <p className={style.card__name}>Lily-Grace Colley</p>
+                    <p className={style.card__name}>Xaropinho</p>
                     <div className={style.grid_container}>
 
                         <div>
@@ -19,13 +21,13 @@ export default function SobreNos() {
 
                     </div>
                     <ul className={style.social_icons}>
-                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i></i></a></li>
+                        <li><a href="#"><i><FontAwesomeIcon icon={faGithub} /></i></a></li>
                         <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
                         <li><a href="#"><i className="fa fa-codepen"></i></a></li>
                     </ul>
-                    <button className="btn draw-border">Follow</button>
-                    <button className="btn draw-border">Message</button>
+                    <button className={`${style.btn} ${style.draw_border}`} >Follow</button>
+                    <button className={`${style.btn} ${style.draw_border}`}>Message</button>
 
                 </div>
 
