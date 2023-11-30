2e03e102c861ef1fe4514c9892466775595709aa
Amanda dos Santos Silva <123501998+Amandamoonchild@users.noreply.github.com>
2023-11-30 15:18:53 -0300

    feat: salvamdo sobrenos

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -5,21 +5,15 @@ import axios from "axios";
 import Header from '../components/header/Header';
 import Members from '../components/members/Members';
 import Footer from '../components/footer/Footer';
-import PopupMessage from '../components/popup/PopUp';
-import InputMembers from '../components/inputmembers/InputMembers';
 
 export default function SobreNos() {
     //area de state
     const [members, setMembers] = useState('');
     const [name, setName] = useState("");
-    const [age, setAge] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
     const [github, setGithub] = useState("");
     const [instagram, setInstagram] = useState("");
-    const [showPopup, setShowPopup] = useState(false);
-    const [popupMessage, setPopupMessage] = useState('');
-    const [popupType, setPopupType] = useState('');
 
     //area de funções
     const deleteMember = async (id) => {
@@ -27,28 +21,25 @@ export default function SobreNos() {
         try {
             await axios.delete(url);
             setMembers(dados.filter((member) => member.id !== id));
-            handleShowPopup('Membro deletado com sucesso', 'success')
         } catch (error) {
-            handleShowPopup(`${error}`, 'error')
+            console.error("error deleting member", error);
         }
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        console.log(name, description, image, github, instagram);
         try {
-            const response = await axios.post("/api/members", { name, age, description, image, github, instagram });
+            const response = await axios.post("/api/members", { name, description, image, github, instagram });
             console.log(response.data);
             setMembers([...members, response.data.data]);
             setName("");
-            setAge("");
             setDescription("");
             setImage("");
             setGithub("");
             setInstagram("");
-            handleShowPopup(`Membro adicionado com sucesso`, 'success')
         } catch (error) {
             console.error(error);
-            handleShowPopup(`${error}`, 'error')
         }
     }
 
@@ -65,15 +56,6 @@ export default function SobreNos() {
         getMembers();
     }, [deleteMember, handleSubmit]);
 
-    //area popUp
-    const handleShowPopup = (message, type,) => {
-        setPopupMessage(message)
-        setPopupType(type)
-        setShowPopup(true)
-        setTimeout(() => {
-            setShowPopup(false)
-        }, 4000)
-    }
 
     //area de retorno
 
@@ -81,24 +63,49 @@ export default function SobreNos() {
         <div className={style.bckg}>
             <Header />
 
-
                 <div className={style.container}>
                     <h1 className={style.title}>Sobre Nós</h1>
                     <p className={style.text}>Aqui você pode encontrar um pouco sobre nós, os integrantes do grupo!</p>
                 </div>
 
-
-            <form onSubmit={handleSubmit} className={style.form}>
-            <InputMembers name={name} setName={setName} age={age} setAge={setAge} description={description} setDescription={setDescription} image={image} setImage={setImage} github={github} setGithub={setGithub} instagram={instagram} setInstagram={setInstagram} />
-            <button className={style.btn} type="submit" onClick={handleSubmit}>Adicionar</button>
-            </form>
+                <form onSubmit={handleSubmit} className={style.form}>
+                    <h3 className={style.form__title}>Adicionar Membro</h3>
+                    <div className={style.form__container}>
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Descrição"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Imagem"
+                            value={image}
+                            onChange={(e) => setImage(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Github"
+                            value={github}
+                            onChange={(e) => setGithub(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Instagram"
+                            value={instagram}
+                            onChange={(e) => setInstagram(e.target.value)}
+                        />
+                        <button className={style.btn} type="submit" onClick={handleSubmit}>Adicionar</button>
+                        </div>
+                    </form>
 
             <Members dados={members} onDelete={deleteMember} />
-            {
-                showPopup ? (
-                    <PopupMessage message={popupMessage} type={popupType} />
-                ) : null
-            }
             <Footer className={style.footerfixed} />
         </div>
     )
